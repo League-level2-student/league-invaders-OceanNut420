82318543ff1b9b9566a335e0c4c83cad7fc2a243
League Student <league@iMac-14.attlocal.net>
2022-09-16 18:25:04 -0700

    oughmaigawd owuaiye aye

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,28 @@
+import java.util.Random;
 
 public class ObjectManager {
 
+	int rocket;
+	int[] projectiles = {};
+	int[] aliens = {};
+	Random rand = new Random();
+	
+	public ObjectManager(int newrocket) {
+		
+		rocket = newrocket;
+		
+	}
+	
+	void addProjectile(int[] newprojectiles) {
+		
+		projectiles = newprojectiles;
+		
+	}
+	
+	void addAlien() {
+		
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+		
+	}
+	
 }
